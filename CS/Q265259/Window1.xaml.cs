062892209ee4f18b2e926e0ba810eab7aae12b9b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Q265259/Window1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using Northwind;
+using DevExpress.Xpo;
+using System.ComponentModel;
+
+namespace XPCollectionSample {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 :Window {
+        public Window1() {
+            InitializeComponent();
+            vm = new MyViewModel();
+            DataContext = vm;
+           
+        }
+        MyViewModel vm;
+   
+
+        protected override void OnClosing(CancelEventArgs e) {
+            base.OnClosing(e);
+            vm.Close();
+        }
+    }
+
+    public class MyViewModel {
+        public MyViewModel() {
+            LoadData();
+        }
+        Session session;
+        public XPCollection<Order> Orders { get; set; }
+        private void LoadData() {
+            session = new Session();
+            Orders = new XPCollection<Order>(session);
+            Orders.DeleteObjectOnRemove = true;
+            Orders.DisplayableProperties = "ShipName;ShipAddress";
+        }
+
+        public void Close() {
+            if (Orders != null) {
+                Orders.Dispose();
+                Orders = null;
+            }
+        }
+    }
+}
---
+++ VB/Q265259/Window1.xaml.vb
@@ -0,0 +1,49 @@
+﻿Imports System
+Imports System.Windows
+Imports Northwind
+Imports DevExpress.Xpo
+Imports System.ComponentModel
+
+Namespace XPCollectionSample
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            vm = New MyViewModel()
+            DataContext = vm
+
+        End Sub
+        Private vm As MyViewModel
+
+
+        Protected Overrides Sub OnClosing(ByVal e As CancelEventArgs)
+            MyBase.OnClosing(e)
+            vm.Close()
+        End Sub
+    End Class
+
+    Public Class MyViewModel
+        Public Sub New()
+            LoadData()
+        End Sub
+        Private session As Session
+        Public Property Orders() As XPCollection(Of Order)
+        Private Sub LoadData()
+            session = New Session()
+            Orders = New XPCollection(Of Order)(session)
+            Orders.DeleteObjectOnRemove = True
+            Orders.DisplayableProperties = "ShipName;ShipAddress"
+        End Sub
+
+        Public Sub Close()
+            If Orders IsNot Nothing Then
+                Orders.Dispose()
+                Orders = Nothing
+            End If
+        End Sub
+    End Class
+End Namespace
